a15f34140bef22846518ccd20bd22c8774a44082
Hoang Cong Ly <congglyyy@gmail.com>
2025-03-19 16:16:51 +0700

    func

---
+++ function/function.cpp
@@ -0,0 +1,10 @@
+// function.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int add(int a, int b)
+{
+	return a + b;
+}
