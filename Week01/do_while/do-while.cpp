b510135d6093cfa85b45b46fb50f442aa3b4ef92
Hoang Cong Ly <congglyyy@gmail.com>
2025-03-19 21:06:57 +0700

    W01

---
+++ Week01/do_while/do-while.cpp
@@ -0,0 +1,13 @@
+// do-while.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+	int num = 1;
+	do {
+		cout << num << " ";
+		num++;
+	} while (num <= 5);
+	return 0;
+}
---
+++ Week01/while/while.cpp
@@ -0,0 +1,14 @@
+// while.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+ using namespace std;
+
+int main() {
+	int num = 1;
+	while (num <= 5) {
+		cout << num << " ";
+			num++;
+	}
+	return 0;
+}
