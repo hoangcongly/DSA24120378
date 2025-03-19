76ee45e6d0b23c6894943e34dd5b6a27d740f032
Hoang Cong Ly <congglyyy@gmail.com>
2025-03-19 16:18:31 +0700

    pointer

--- pointers/pointers.cpp
+++ pointers/pointers.cpp
@@ -3,18 +3,13 @@
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+int main() {
+	int x = 10;
+	int* ptr = &x; // Pointer storing the address of x
+	cout << "Value of x: " << x << endl; // Output: 10
+	cout << "Address of x: " << &x << endl; // Memory address
+	cout << "Pointer value: " << ptr << endl; // Address stored in ptr
+	cout << "Value using pointer: " << *ptr << endl; // Dereferencing: 10
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
